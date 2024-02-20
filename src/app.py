88421b703f93cfe208b19b97b1cd9258572d934c
Jonatan Rico <jonatancalebrico@gmail.com>
2024-02-20 17:17:13 -0600

    Simple toDo finished

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
+import os
 import datetime
 
 # Empty diccionary
 db = {"id":[],"task":[],"created_at":[]}
 
 index = 0
+
+
 def addTask():
     global index
     task = input("Please introduce the task that you'll do: ")
@@ -13,20 +16,45 @@ def addTask():
         db["id"].append(index)
         db["task"].append(task)
         db["created_at"].append(time)
+        os.system('clear')
         print("Task created")
         index +=1
     except: 
         print("Something went wrong in the addition of this task")
-    print(db)
+    # print(db)
 
 def editTask(idTask):
     global index
     editedTask = input("Introduce the task you want to replace with: ")
-    db['task'][idTask] = editedTask
-    print(db['task'][idTask])
+    os.system('clear')
+    try: 
+        db['task'][idTask] = editedTask
+        print(db['task'][idTask])
+    except:
+        print("Something fail while editing the task, FIX IT!!")
+
+def showOneTask(searchId):
+    int(searchId)
+    oneTaskId = db["id"][searchId]
+    oneTaskTask = db["task"][searchId]
+    oneTaskDate= db["created_at"][searchId]
+    wholeTaskArray = [oneTaskId, oneTaskTask, oneTaskDate]
+    os.system('clear')
+    print(wholeTaskArray)
+
+def deleteOneTask(idToDelete):
+    try:
+        del db["id"][idToDelete],db["created_at"][idToDelete],db["task"][idToDelete]
+        os.system('clear')
+        print("Task succesfully deleted")
+    except:
+        print("Something fail while deleting this task")
+
+def deleteAllTasks():
+    db = {"id":[],"task":[],"created_at":[]}
+    print("Entire tasks have been succesfully deleted =)")
 
 while True:
-
 
     print("(1). Add Task")
     print("(2). Edit task")
@@ -48,13 +76,18 @@ while True:
                     print("Show all tasks")
                     print(db)
                 case "4":
-                    print("Ask for the task id to show it")
+                    searchIdTask= int(input("Introduce the id of the task please: "))
+                    showOneTask(searchIdTask)
                 case "5":
-                    print("Ask for the task id to delete")
+                    idToDelete = int(input("Ask for the task id to delete"))
                 case "6":
-                    print("Ask for confirmation to erase all the tasks")
+                    confirmation = input("Type 'Yes' if your sure to delete all the tasks. Anything else will cancel the request: ")
+                    if(confirmation == "Yes" or confirmation == "yes"):
+                            deleteAllTasks()
+                    else:
+                        print("Task deletion canceled")
                 case "7":
                     print("Goodbye, see you soon")
                     exit()
-                
-    print("Invalid option. Please choose an option from 1 to 6")
+                case _:
+                    print("Invalid option. Please choose an option from 1 to 6")
